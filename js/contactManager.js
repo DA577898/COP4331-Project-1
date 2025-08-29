e6d0a459e9a781bd3956e37eb704297b99bedf0b
Anju <124937896+anjunthomas@users.noreply.github.com>
2025-08-29 13:14:31 -0400

    Update contactManager.js

---
+++ js/contactManager.js
@@ -0,0 +1,41 @@
+
+
+// need to create a form for this information
+document.querySelector('.add-contact-button').addEventListener('click', function() {
+    const firstName = document.getElementById('inputFirstName').value;
+    const lastName = document.getElementById('inputLastName').value;
+    const email = document.getElementById('inputEmail').value;
+    const phoneNumber = document.getElementById('inputPhoneNumber').value;
+    
+    // this should be stored from login session 
+    // const userId = localStorage.getItem('userId');
+    
+    fetch('/LAMPAPI/CreateContact.php', { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            userId: parseInt(userId),       
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phoneNumber: phoneNumber
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.error === "") {  
+           // adding to contact here if response was successful
+           // clearing the form
+           console.log("response was successful");
+        } else {
+            // Show error message
+            console.log("there was an error")
+        }
+    })
+    .catch(error => {
+        alert('Failed to add contact. Please try again.');
+        console.error('Error:', error);
+    });
+});
